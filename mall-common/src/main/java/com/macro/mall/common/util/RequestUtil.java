55533133e09305863c02218fb285c2490c7d6da6
luoxiongbo <3160821850@qq.com>
2024-11-10 08:49:41 +0800

    更新

---
+++ mall-common/src/main/java/com/macro/mall/common/util/RequestUtil.java
@@ -0,0 +1,47 @@
+package com.macro.mall.common.util;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * 请求工具类
+ * Created by macro on 2020/10/8.
+ */
+public class RequestUtil {
+
+    /**
+     * 获取请求真实IP地址
+     */
+    public static String getRequestIp(HttpServletRequest request) {
+        //通过HTTP代理服务器转发时添加
+        String ipAddress = request.getHeader("x-forwarded-for");
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+            // 从本地访问时根据网卡取本机配置的IP
+            if (ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")) { // 这是一个IPV6地址，它等价于 IPv4 地址中的 “127.0.0.1”，通常被称为 IPv6 环回地址
+                InetAddress inetAddress = null;
+                try {
+                    inetAddress = InetAddress.getLocalHost(); // 根据网卡取本机配置的 IP
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                ipAddress = inetAddress.getHostAddress();
+            }
+        }
+        // 通过多个代理转发的情况，第一个IP为客户端真实IP，多个IP会按照','分割
+        if (ipAddress != null && ipAddress.length() > 15) {
+            if (ipAddress.indexOf(",") > 0) {
+                ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+            }
+        }
+        return ipAddress;
+    }
+
+}
